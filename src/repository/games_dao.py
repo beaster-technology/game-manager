d91927ad15721f7486196d02cbdc797b1cd304f9
Benicio Januario <benicio.januario@ifood.com.br>
2022-07-02 10:57:15 -0300

    feat: build DAOs and fix their names cases

---
+++ src/repository/games_dao.py
@@ -0,0 +1,62 @@
+from copy import deepcopy
+
+from model.game import Game
+from model.player import Player
+from model.bet import Bet
+
+MOCKED_EPOCH = 1656681396.448879
+MOCKED_GAME_LIST = [
+    Game(
+        teams=('Brasil', 'França'),
+        players=[
+            Player('Tanga', Bet(10, MOCKED_EPOCH)),         # Born along its game
+            Player('Beni', Bet(10, MOCKED_EPOCH + 180)),    # Born 3 minutes after its game was created
+            Player('Lusca', Bet(10, MOCKED_EPOCH + 120)),   # Born 2 minutes after its game was created
+            Player('Tchan', Bet(10, MOCKED_EPOCH + 600))    # Born 10 minutes after its game was created
+        ],
+        open_at=MOCKED_EPOCH,
+    ),
+    Game(
+        teams=('Argentina', 'Itália'),
+        players=[
+            Player('Lusni', Bet(10, MOCKED_EPOCH + 3600 + 600)),    # Born 10 minutes after its game was created
+            Player('Tchanga', Bet(10, MOCKED_EPOCH + 3600)),        # Born along its game
+            Player('Besca', Bet(10, MOCKED_EPOCH + 3600 + 120)),    # Born 2 minutes after its game was created
+            Player('Tanan', Bet(10, MOCKED_EPOCH + 3600 + 180))     # Born 3 minutes after its game was created
+        ],
+        unit='Bandecos',
+        open_at=MOCKED_EPOCH + 3600, # Born 1 hour after previous game
+    ),
+]
+
+class GamesDAO: # This guy needs to connect to firebase firestore
+    def __init__(self) -> None:
+        # Here you can initialize the connection
+        pass
+
+    def list(open_only: bool = True):
+        # Retrieve all games
+        return MOCKED_GAME_LIST
+
+    def retrieve(id: str):
+        # Retrieve game with id
+        target_game: Game = deepcopy(MOCKED_GAME_LIST[0])
+        target_game.id = id
+
+        return target_game
+
+    def insert(game: Game):
+        # Insert provided game in firestore
+        pass
+
+    def update(game: Game):
+        # Update provided game in firestore
+        pass
+
+    def delete(id: str):
+        # Delete game result with provided ID
+        pass
+
+    def close(id: str):
+        # Close game with provided ID - set is_open to False
+        pass
---
+++ src/repository/results_dao.py
@@ -0,0 +1,26 @@
+from copy import deepcopy
+from model.result import Result
+from model.player import Player
+from model.bet import Bet
+
+MOCKED_EPOCH = 1656681396.448879
+MOCKED_RESULT = Result('61be0f5f-a5c1-4e0a-92ef-686eac1e3699', [
+    Player('Tanan', Bet(50, MOCKED_EPOCH)),
+    Player('Besca', Bet(35, MOCKED_EPOCH))
+])
+
+class ResultsDAO: # This guy needs to connect to firebase firestore
+    def __init__(self) -> None:
+        # Here you can initialize the connection
+        pass
+
+    def retrieve(id: str):
+        # Retrieve targeted result
+        target_result: Result = deepcopy(MOCKED_RESULT)
+        target_result.id = id
+        
+        return target_result
+
+    def delete(id: str):
+        # Delete game result with provided ID
+        pass

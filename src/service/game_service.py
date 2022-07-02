a5fd11b82d9cc32af767140c26970c660fe17c23
Benicio Januario <benicio.januario@ifood.com.br>
2022-07-02 10:58:42 -0300

    feat: move mocks to repository layer and abstract services

--- src/service/game_service.py
+++ src/service/game_service.py
@@ -1,125 +1,83 @@
+from uuid import UUID
 from model.game import Game
 from model.player import Player
 from model.bet import Bet
-from model.result import Result
 
 from serializer.game_serializer import GameSerializer
 from serializer.player_serializer import PlayerSerializer
-from serializer.result_serializer import ResultSerializer
 
-MOCKED_EPOCH = 1656681396.448879
-MOCKED_GAME_LIST = [
-    Game(
-        teams=('Brasil', 'França'),
-        players=[
-            Player('Tanga', Bet(10, MOCKED_EPOCH)),         # Born along its game
-            Player('Beni', Bet(10, MOCKED_EPOCH + 180)),    # Born 3 minutes after its game was created
-            Player('Lusca', Bet(10, MOCKED_EPOCH + 120)),   # Born 2 minutes after its game was created
-            Player('Tchan', Bet(10, MOCKED_EPOCH + 600))    # Born 10 minutes after its game was created
-        ],
-        open_at=MOCKED_EPOCH,
-    ),
-    Game(
-        teams=('Argentina', 'Itália'),
-        players=[
-            Player('Lusni', Bet(10, MOCKED_EPOCH + 3600 + 600)),    # Born 10 minutes after its game was created
-            Player('Tchanga', Bet(10, MOCKED_EPOCH + 3600)),        # Born along its game
-            Player('Besca', Bet(10, MOCKED_EPOCH + 3600 + 120)),    # Born 2 minutes after its game was created
-            Player('Tanan', Bet(10, MOCKED_EPOCH + 3600 + 180))     # Born 3 minutes after its game was created
-        ],
-        unit='Bandecos',
-        open_at=MOCKED_EPOCH + 3600, # Born 1 hour after previous game
-    ),
-]
-MOCKED_RESULT = Result('61be0f5f-a5c1-4e0a-92ef-686eac1e3699', [
-    Player('Tanan', Bet(50, MOCKED_EPOCH)),
-    Player('Besca', Bet(35, MOCKED_EPOCH))
-])
+from repository.games_dao import GamesDAO
+from repository.players_dao import PlayersDAO
+
+from model.exceptions.invalid_uuid import InvalidUUID
+from model.exceptions.resource_not_found import ResourceNotFound
+from model.exceptions.invalid_request_payload import InvalidRequestPayload
 
 class GameService:
 
     def list_games(open_only: bool=True):
-        # Get list from repository
-        return GameSerializer.serialize_list(MOCKED_GAME_LIST)
+        games: list[Game] = GamesDAO.list(open_only)
+        return GameSerializer.serialize_list(games)
 
     def get_game(id: str):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
 
-        # TODO: Search in repository and raise unhandled exceptions - Exception
-        mocked_game = MOCKED_GAME_LIST[0]
-        mocked_game.id = id
+        game = GamesDAO.retrieve(id)
+        if not game: raise ResourceNotFound
 
-        # Check zero value from repo and check for it at the controller
-
-        return GameSerializer.serialize(mocked_game)
+        return GameSerializer.serialize(game)
 
     def get_players(id: str):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
         
-        # TODO: Search in repository and raise unhandled exceptions - Exception
+        players = PlayersDAO.retrieve(id)
+        if players == '': raise ResourceNotFound
 
-        # Check game existance from repo and check for it here - NotFoundException
-
-        return PlayerSerializer.serialize_list(MOCKED_GAME_LIST[0].players)
-
-    def get_result(id: str):
-        # TODO: Validate UUID formatting - InvalidUUID
-        
-        # TODO: Search in repository and raise unhandled exceptions - Exception
-
-        # Check game existance from repo and check for it here - NotFoundException
-
-        return ResultSerializer.serialize(MOCKED_RESULT)
+        return PlayerSerializer.serialize_list(players)
 
     def create_game(request):
 
         # Validate body
 
-        # TODO: Create game in repository and raise unhandled exceptions - Exception
+        # Generate game creation class
+        game = Game(('Brasil', 'Espanha'), [])
 
+        GamesDAO.insert(game)
 
-        return GameSerializer.serialize(MOCKED_GAME_LIST[0])
+        return GameSerializer.serialize(game)
 
     def close_game(id):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
 
-        # TODO: Search in repository and raise unhandled exceptions - Exception
-
-        # Make sure game exists - NotFoundException
-
-        # TODO: Check if game is already closed - AlreadyClosedGame
-
-        # Close game
-
-        pass
+        GamesDAO.close(id)
 
     def update_game(id, request):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
 
-        # Make sure game exists - NotFoundException
+        # Validate game update payload
 
-        # TODO: Update in repository and raise unhandled exceptions - Exception
+        # Build game object
+        game_update = Game(('Brasil', 'Espanha'), [])
 
-        # Update the game
-
-        pass
+        GamesDAO.update(game_update)
 
     def update_players(id, request):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
 
-        # Make sure game exists - NotFoundException
+        # Validate players update payload
 
-        # TODO: Update game in repository and raise unhandled exceptions - Exception
+        # Build players objects list
+        game_update = Player('Besca', Bet(10))
 
-        pass
+        PlayersDAO.update(game_update)
 
     def delete_game(id):
-        # TODO: Validate UUID formatting - InvalidUUID
+        try: _ = UUID(id, version=4)
+        except ValueError: raise InvalidUUID
 
-        # TODO: Search in repository and raise unhandled exceptions - Exception
-
-        # Make sure game exists - NotFoundException
-
-        # Delete targeted game
-
-        pass
+        GamesDAO.delete(id)

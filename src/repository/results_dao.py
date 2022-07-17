2c5e140e68648f71fa881d1a808c01cdd9f94adc
Henrique Bovo <henrique.bovo@usp.br>
2022-07-17 16:13:09 -0300

    feat: connection with firestore was established

--- src/repository/results_dao.py
+++ src/repository/results_dao.py
@@ -1,10 +1,13 @@
-from copy import deepcopy
-
+import os
+from pydoc import doc
+from xml.dom.minidom import Document
 from src.model.game import Game
 from src.model.result import Result
 from src.model.player import Player
 from src.model.bet import Bet
 from google.cloud import firestore
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='/mnt/c/Users/henri/Meu Drive/Drive Pessoal/Trabalhos/2022.1/POO/Python/game-manager/credentials/beaster-f041b-1f5e0e12c487.json'
 
 MOCKED_EPOCH: float = 1656681396.448879
 MOCKED_RESULT: list[Game] = Result('61be0f5f-a5c1-4e0a-92ef-686eac1e3699', [
@@ -13,18 +16,20 @@ MOCKED_RESULT: list[Game] = Result('61be0f5f-a5c1-4e0a-92ef-686eac1e3699', [
 ], 'Brazil')
 
 class ResultsDAO: # This guy needs to connect to firebase firestore
-    def __init__(self) -> None:
-        # Set variable type from collection return type
-        self.results_collection = firestore \
-            .Client(project = 'beaster-f041b') \
-            .collection(u'results')
+    db = firestore.Client(project='beaster-f041b').collection('results')
 
-    def retrieve(self, id: str) -> Result:
-        print(self.results_collection.document(id))
+    @staticmethod
+    def retrieve(id: str) -> Result:
+        document = ResultsDAO.db.document(id).get()
+        if not document: return None
 
-    def insert(self, result: Result) -> None:
+        print(document.to_dict())
+      
+    @staticmethod
+    def insert(result: Result) -> None:
         
         pass
 
+    @staticmethod
     def delete(id: str) -> None:
         pass
